8227987b6592bec42d171250d2a34ab90620c706
Guillaume LEDUC <82090419+ElDucche@users.noreply.github.com>
2025-06-26 20:00:35 +0000

    Début intégration test e2e sur page account.

--- front/cypress/e2e/account.cy.ts
+++ front/cypress/e2e/account.cy.ts
@@ -1,12 +1,25 @@
-// describe('Account spec', () => {
-//     beforeEach(() => {
-//         cy.visit('/login')
-//         cy.intercept('GET', '/api/user/1', {
-//             body: [
-//                 {
-//                     id: 1,
-//                     firstName: 'John',
-//                     lastName: 'Doe',
-//                     email: 'test@yoga.zyx',
-//                 }]
-//             })
+describe('Account spec', () => {
+    beforeEach(() => {
+        cy.visit('/login')
+        
+
+        cy.intercept('POST', '/api/auth/login', {
+            body: {
+                id: 1,
+                username: 'userName',
+                firstName: 'firstName',
+                lastName: 'lastName',
+                admin: true
+            },
+        })
+        cy.intercept('GET', '/api/user/1', {
+            body: [
+                {
+                    id: 1,
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'test@yoga.zyx',
+                }]
+        })
+    })
+})
